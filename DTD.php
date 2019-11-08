fc6af07885a860859c63049a24f9d7aa99d477cc
MarqCervMartin <martin_mcervantes@hotmail.com>
2019-11-07 19:31:45 -0600

    Maldito PHP, pero ya quedo

---
+++ DTD.php
@@ -0,0 +1,21 @@
+<?php
+    require("config.php");
+    header("Content-type: text / xml");
+    $enlace = new mysqli($host,$user,$pass) or die ("Error MySQL");
+    mysqli_select_db($enlace,$database) or die ("Error en la Base de Datos");
+    $query = "SELECT * FROM productos ORDER BY id ASC";
+    $resultado = $enlace->query($query);
+    $salida_xml = "<?xml version=\"1.0\"?>";
+    $salida_xml.= "<almacen>\n";
+    //mysqli retornara el numero de filas correcto hasta que todas las filas del resultado hayan sido recuperadas.
+    for($x=0;$x<mysqli_num_rows($resultado);$x++){
+        $fila = $resultado->fetch_assoc();
+    $salida_xml.= "\t<Producto>\n";
+    $salida_xml.= "\t<Nombre>".$fila['Nombre']."</Nombre>\n";
+    $salida_xml.= "\t<Precio>".$fila['Precio']."</Precio>\n";
+    $salida_xml.= "\t<Categoria>".$fila['Categoria']."</Categoria>\n";
+    $salida_xml.= "\t<Marca>".$fila['Marca']."</Marca>\n";
+    }
+    $salida_xml.="</almacen>";
+    echo $salida_xml;
+?>
